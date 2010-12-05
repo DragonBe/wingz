e57ce141c0d72f6a01a03d2603e3a3e0165573c1
Michelangelo <dragonbe+github@gmail.com>
2010-12-05 20:35:52 +0100

    providing basic layout and navigation

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,33 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initView()
+    {
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini',
+            APPLICATION_ENV);
+        // Initialize view
+        $view = $this->getPluginResource('view')->getView();
+        $view->doctype('XHTML1_STRICT');
+        $view->headTitle(sprintf('%s v%s', $config->app->name, $config->app->version));
+        $view->headLink()->appendStylesheet($view->baseUrl('/css/style.css'));
+        $view->headMeta()->setHttpEquiv('text/html; Charset=UTF-8', 'Content-Type');
+        // Add it to the ViewRenderer
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+            'ViewRenderer'
+        );
+        $viewRenderer->setView($view);
+        // Return it, so that it can be stored by the bootstrap
+        return $view;
+    }
+    
+    protected function _initNavigation()
+    {
+        $config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/navigation.xml');
+        $navigation = new Zend_Navigation();
+        $navigation->setPages($config->nav->toArray());
+        $view = $this->getPluginResource('view')->getView();
+        $view->navigation($navigation);
+        return $view;
+    }
 }
 
