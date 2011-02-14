5123a6e0acca29b320d1c46ad2d2fe92879ac4e0
Michelangelo <dragonbe+github@gmail.com>
2011-02-14 09:55:33 +0100

    Updating files with proper header for documentation purposes

--- library/Wingz/Service/Exception.php
+++ library/Wingz/Service/Exception.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Wingz: Write PHP, deploy anywhere
+ * 
+ * Wingz is an example application that uses a fully working Zend Framework
+ * application that can run on Linux w/ Apache, Microsoft Windows w/ IIS and
+ * on Microsoft Windows Azure w/ IIS.
+ * 
+ * @license		CreativeCommons-Attribution-ShareAlike
+ * @link        http://creativecommons.org/licenses/by-sa/3.0/
+ * @category	Wingz
+ */
 class Wingz_Service_Exception extends Wingz_Exception
 {
     
