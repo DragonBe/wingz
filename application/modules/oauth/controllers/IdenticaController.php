54097b0f5a43ff286816399d34a467befed7bab2
Michelangelo <dragonbe+github@gmail.com>
2011-03-13 01:45:56 -0500

    Providing simple messaging service for Identi.ca

--- application/modules/oauth/controllers/IdenticaController.php
+++ application/modules/oauth/controllers/IdenticaController.php
@@ -12,34 +12,53 @@ class Oauth_IdenticaController extends Zend_Controller_Action
     public function indexAction()
     {
         $accessToken = $userName = $screenName = null;
-        if (isset ($this->_session->identica_access_token)) {
-            $accessToken = unserialize($this->_session->identica_access_token);
+        if (isset ($this->_session->identity)) {
+            $identica = new Wingz_Service_Identica_Client(
+                $this->_session->identity, new Zend_Http_Client());
+            $response = $identica->accountVerifyCredentials();
+            $status = $identica->statusesUserTimeline();
+            $version = $identica->statusnetGetVersion();
             
-            $identica = new Wingz_Service_Identica(array ('accessToken' => $accessToken));
-            $account = $identica->accountVerifyCredentials();
+            $form = $this->_getMessageForm();
             
-            if (!isset ($this->_session->identica_user_name)) {
-                $userName = (string) $account->name;
-                $this->_session->identica_user_name = $userName;
-            } else {
-                $userName = $this->_session->identica_user_name;
+            if (isset ($this->_session->messageForm)) {
+                $form = unserialize($this->_session->messageForm);
+                unset ($this->_session->messageForm);
             }
             
-            if (!isset ($this->_session->identica_screen_name)) {
-                $screenName = (string) $account->screen_name;
-                $this->_session->identica_screen_name = $screenName;
-            } else {
-                $screenName = $this->_session->identica_screen_name;
-            }
+            $this->view->assign(array (
+                'screenName' => $response->screen_name,
+                'userName' => $response->name,
+                'status' => $status,
+                'version' => $version,
+                'form' => $form,
+            ));
         }
-        
-        $this->view->accessToken = $accessToken;
-        $this->view->screenName = $screenName;
-        $this->view->userName = $userName;
     }
 
     public function authAction()
     {
+        if (!$this->getRequest()->isPost()) {
+            return $this->_helper->redirector('login', 'identica', 'oauth');
+        }
+        $form = $this->_getForm();
+        if (!$form->isValid($this->getRequest()->getPost())) {
+            $this->_session->form = serialize($form);
+            return $this->_helper->redirector('login', 'identica', 'oauth');
+        }
+        
+        $values = $form->getValues();
+        $identica = new Wingz_Service_Identica_Client(
+            $values, new Zend_Http_Client());
+        $response = null;
+        try {
+            $response = $identica->accountVerifyCredentials();
+        } catch (Wingz_Service_Identica_Exception $e) {
+            $this->_session->form = serialize($form);
+            return $this->_helper->redirector('login', 'identica', 'oauth');
+        }
+        $this->_session->identity = $values;
+        /*
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/api.ini', APPLICATION_ENV);
         $identicaConfig = $config->identica->toArray();
         
@@ -54,12 +73,72 @@ class Oauth_IdenticaController extends Zend_Controller_Action
 //            Zend_Debug::dump($requestToken);die;
             $this->_session->identica_request_token = serialize($requestToken);
             $consumer->redirect();
-        }
+        }*/
         return $this->_helper->redirector('index', 'identica', 'oauth');
+    }
+
+    public function loginAction()
+    {
+        $form = $this->_getForm();
+        
+        if (isset ($this->_session->form)) {
+            $form = unserialize($this->_session->form);
+            unset ($this->_session->form);
+        }
+        $this->view->form = $form;
+    }
+
+    public function messageAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->_helper->redirector('index', 'identica', 'oauth');
+        }
+        $form = $this->_getMessageForm();
+        if (!$form->isValid($this->getRequest()->getPost())) {
+            $this->_session->messageForm = serialize($form);
+            return $this->_helper->redirector('index', 'identica', 'oauth');
+        }
+        $values = $form->getValues();
+        $identica = new Wingz_Service_Identica_Client(
+            $this->_session->identity, new Zend_Http_Client());
+        $response = $identica->statusesUpdate($values['message'], 
+            array ('lat' => $values['latitude'], 'long' => $values['longitude']));
+        unset ($this->_session->messageForm);
+        return $this->_helper->redirector('index', 'identica', 'oauth');
+    }
+
+    protected function _getForm()
+    {
+        $form = new Oauth_Form_Identica(array (
+        	'action' => $this->view->url(array (
+                'module' => 'oauth',
+                'controller' => 'identica',
+                'action' => 'auth',
+            )),
+            'method' => 'post',
+        ));
+        return $form;
+    }
+
+    protected function _getMessageForm()
+    {
+        $form = new Oauth_Form_IdenticaMessage(array (
+        	'action' => $this->view->url(array (
+                'module' => 'oauth',
+                'controller' => 'identica',
+                'action' => 'message',
+            )),
+            'method' => 'post',
+        ));
+        return $form;
     }
 
 
 }
+
+
+
+
 
 
 
