a07d4283c8edc32138733dc2b611c915e7865396
Michelangelo <dragonbe+github@gmail.com>
2011-07-10 00:15:08 +0200

    issue #1: removing number from variable (http://192.168.66.23:8080/job/wingz/violations/file/library/Wingz/Service/Joindin2/Interface.php/?#line4)

--- library/Wingz/Service/Joindin2/Interface.php
+++ library/Wingz/Service/Joindin2/Interface.php
@@ -1,6 +1,6 @@
 <?php
 interface Wingz_Service_Joindin2_Interface
 {
-    public function setJoindin(Wingz_Service_Joindin2 $joindin2);
+    public function setJoindin(Wingz_Service_Joindin2 $joindin);
     public function getJoindin();
 }
